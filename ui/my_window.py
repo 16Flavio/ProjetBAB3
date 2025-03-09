3b33ee1cc47cd60d75244c709ac819865ee3ad99
Flavio Drogo <230736@umons.ac.be>
2025-03-09 17:12:07 +0100

    Initial commit - première version du projet

---
+++ ui/my_window.py
@@ -0,0 +1,45 @@
+from PyQt5 import QtWidgets
+from main_window import Ui_MainWindow
+import sys
+import os
+
+# Si 1ère fois que main_window est import depuis Qt Designer, il faut :
+# - Changer le chemin de l'icone de l'app comme ceci
+"""
+from pathlib import Path
+
+# Chemin absolu vers l'icône
+ressources_path = Path(__file__).parent.parent / "ressources"
+
+icon.addPixmap(QtGui.QPixmap(str(ressources_path) + "/AppIcon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+"""
+
+class myWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(myWindow, self).__init__()
+        self.setupUi(self)
+
+        # Connexions
+        self.toggleBtn.clicked.connect(self.toggle_menu)
+        self.manualMode.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(0))
+        self.autoMode.clicked.connect(lambda: self.stackedWidget.setCurrentIndex(1))
+        
+        # Initialisation
+        self.sidebar_width = 200
+        self.menu_visible = True
+
+    def toggle_menu(self):
+        self.menu_visible = not self.menu_visible
+        self.sideBar.setFixedWidth(self.sidebar_width if self.menu_visible else 0)
+
+app = QtWidgets.QApplication(sys.argv)
+
+# Chemin relatif au fichier Python
+css_path = os.path.join(os.path.dirname(__file__), 'style.css')
+with open(css_path, 'r') as f:
+    app.setStyleSheet(f.read())
+
+window = myWindow()
+window.show()
+
+sys.exit(app.exec())
